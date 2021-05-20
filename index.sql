c5bfdeafa04d1b5b7840e3fda0114f12d87253bd
vhieu-nx <hieunvbhaf180070@fpt.edu.vn>
2021-05-20 09:46:49 +0700

    procedure

--- index.sql
+++ index.sql
@@ -4070,3 +4070,30 @@ EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
 EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
 ALTER TABLE customers DROP INDEX idx_full_name;
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+  SELECT * FROM customers;
+
+END //
+
+DELIMITER ;
+
+call findAllCustomers();
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+SELECT * FROM customers where customerNumber = 175;
+
+END //
+DELIMITER ;
+
+
+call findAllCustomers();
